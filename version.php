3e63dabbb00291dfe26e7035871740d768bc8c44
Karin Borm <k.borm@ostfalia.de>
2020-03-27 10:28:01 +0100

    #6: return error message for missing files in summarise_response (instead of exception)

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_proforma';
-$plugin->version   = 2020022101;
+$plugin->version   = 2020032700;
 
 $plugin->requires  = 2017111300;
-$plugin->release = '2.1.1';
+$plugin->release = '2.1.2';
 
 $plugin->maturity  = MATURITY_STABLE;
 
