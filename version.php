4ee7c81a7e256e89f29536335796e1b79f0284a9
Karin Borm <k.borm@ostfalia.de>
2020-04-23 10:40:20 +0200

    finalise 2.2.0

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_proforma';
-$plugin->version   = 2020042100;
+$plugin->version   = 2020042300;
 
 $plugin->requires  = 2017111300;
-$plugin->release = '2.2.0 RC2';
+$plugin->release = '2.2.0';
 
 $plugin->maturity  = MATURITY_STABLE;
 
