8d82a1221ad2866162b49939cf721ea17af57e48
Karin Borm <k.borm@ostfalia.de>
2021-02-03 09:24:11 +0100

    test: update jenkins script

--- tests/jenkins/jenkinsfile.groovy
+++ tests/jenkins/jenkinsfile.groovy
@@ -16,6 +16,7 @@ def combinations = [
 pipeline {
     agent { node('WSL') } // run in WSL 2 environment on Windows with Ubuntu 20 installed.
     parameters {
+        choice(name: 'SOURCE_ORIGIN', choices: ['github', 'local'], description: 'Where to get source code from')
         choice(name: 'MOODLE_VERSION', choices: ['all', '38', '39', '310', '311', 'master'], description: 'Run with specific Moodle version')
         choice(name: 'DATABASE_TYPE', choices: ['all', 'mysql', 'pgsql'], description: 'Run with specific database')
         choice(name: 'PHP_VERSION', choices: ['all', '7.1', '7.2', '7.3', '7.4'], description: 'Run with specific PHP version')
@@ -30,14 +31,14 @@ pipeline {
                         // use predefined combinations
                         for (combination in combinations) {
                             stage("Moodle " + combination[0] + " PHP " + combination[1] + " " + combination[2]){
-                                runTest(combination[0], combination[1], combination[2])
+                                runTest(params.SOURCE_ORIGIN, combination[0], combination[1], combination[2])
                             }                    
                         }
                     } else {
                         echo "run specific combination"
                         // use specific versions
                         stage("Moodle ${params.MOODLE_VERSION} PHP_VERSION ${params.PHP} ${params.DATABASE_TYPE}"){
-                            runTest(params.MOODLE_VERSION, params.PHP_VERSION, params.DATABASE_TYPE)
+                            runTest(params.SOURCE_ORIGIN, params.MOODLE_VERSION, params.PHP_VERSION, params.DATABASE_TYPE)
                         }                    
                     }
                 }
@@ -46,14 +47,20 @@ pipeline {
     }
 }
 
-def runTest(String moodle_version, String php_version, String db_type) {
+def runTest(String source_origin, String moodle_version, String php_version, String db_type) {
     // set environment variables for shell scripts
     env.PHP = php_version
     env.DATABASE = db_type
     
+    echo "Source from " + source_origin
     echo "Checkout Moodle " + moodle_version
     echo "Use PHP " + php_version + " and DB " + db_type
     
+    proforma_path="/mnt/e/users/karin/job/ostfalia/git/moodle/"
+    
+    dir('moodle-docker') {
+        git url: 'https://github.com/moodlehq/moodle-docker.git'
+    }    
     dir('moodle') {
         if (moodle_version == "master") {
             branch = moodle_version
@@ -63,24 +70,41 @@ def runTest(String moodle_version, String php_version, String db_type) {
         git branch: branch,
             url: 'https://github.com/moodle/moodle.git'
     }
-    dir('moodle/question/type/proforma') {
-        git url: 'https://github.com/elearning-ostfalia/moodle-qtype_proforma.git'
-    }
-    dir('moodle/question/format/proforma') {
-        git url: 'https://github.com/elearning-ostfalia/moodle-qformat_proforma.git'
-    }
-    dir('moodle/question/behaviour/adaptiveexternalgrading') {
-        git url: 'https://github.com/elearning-ostfalia/moodle-qbehaviour_adaptiveexternalgrading.git'
-    }
-    dir('moodle-docker') {
-        git url: 'https://github.com/moodlehq/moodle-docker.git'
-    }
-    dir('moodle/question/type/proforma/tests/jenkins') {
+    if (source_origin == 'github') {
+        echo 'Getting ProFormA code from github...'        
+        dir('moodle/question/type/proforma') {
+            git branch: 'master',
+                url: 'https://github.com/elearning-ostfalia/moodle-qtype_proforma.git'
+        }
+        dir('moodle/question/format/proforma') {
+            git url: 'https://github.com/elearning-ostfalia/moodle-qformat_proforma.git'
+        }
+        dir('moodle/question/behaviour/adaptiveexternalgrading') {
+            git url: 'https://github.com/elearning-ostfalia/moodle-qbehaviour_adaptiveexternalgrading.git'
+        }
+        dir('moodle/question/type/proforma/tests/jenkins') {
+            echo 'Initialising test...'
+            sh('./jenkins_init.sh')
+            echo 'Starting tests...'
+            sh('./jenkins_run.sh')
+        }        
+    } else {
+        echo 'Copying ProFormA code from local disk...'        
+        dir('moodle') {        
+            sh('rsync -rv --delete-before -q --exclude=.git ' + proforma_path + 'question/behaviour/adaptiveexternalgrading question/behaviour')
+            sh('rsync -rv --delete-before -q --exclude=.git ' + proforma_path + 'question/type/proforma question/type')
+            sh('rsync -rv --delete-before -q --exclude=.git ' + proforma_path + 'question/format/proforma question/format')
+            // Start from local path
+            sh('cp ' + proforma_path + 'question/type/proforma/tests/jenkins/jenkins_init.sh /home/jenkins/jenkins_init_p.sh')
+            sh('cp ' + proforma_path + 'question/type/proforma/tests/jenkins/jenkins_run.sh /home/jenkins/jenkins_run_p.sh')
+        }            
+        echo 'Starting test:'
         echo 'Initialising test...'
-        sh('./jenkins_init.sh')
+        sh('/home/jenkins/jenkins_init_p.sh')
         echo 'Starting tests...'
-        sh('./jenkins_run.sh')
+        sh('/home/jenkins/jenkins_run_p.sh')    
     }
+
     /* echo 'Initialising test...'
     sh('/home/jenkins/jenkins_init.sh')
     echo 'Starting tests...'
